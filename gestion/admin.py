595a51e20d6fdd3e1f5089820db257dfcc0f108d
AlejandroBast <nicohlas.personal@gmail.com>
2026-04-23 07:20:44 -0700

    backend implementado

---
+++ gestion/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import Autor, Libro
+
+
+@admin.register(Autor)
+class AutorAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'correo', 'nacionalidad', 'fecha_nacimiento')
+    search_fields = ('nombre', 'correo')
+
+
+@admin.register(Libro)
+class LibroAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'genero', 'fecha_publicacion', 'autor', 'isbn')
+    list_filter = ('genero', 'autor')
+    search_fields = ('titulo', 'isbn')
